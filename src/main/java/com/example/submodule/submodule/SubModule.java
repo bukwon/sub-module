f992f63b7e00f2a52983f1f40cd78dce799ea7d2
bukwon <dnu05043@gmail.com>
2024-09-12 18:02:06 +0900

    Sub module

---
+++ src/main/java/com/example/submodule/submodule/SubModule.java
@@ -0,0 +1,19 @@
+package com.example.submodule.submodule;
+
+public class SubModule {
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int calc(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int division(int num1, int num2) {
+        return num1 / num2;
+    }
+}
